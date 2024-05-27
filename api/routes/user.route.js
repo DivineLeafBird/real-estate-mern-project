f53dd0c5f9c1de82ba7076824bf216c638d5d940
DivineLeafBird <kassimyahya93@gmail.com>
2024-05-27 08:38:13 +0300

    feat: Add test route and controller for user API

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,8 @@
 import express from "express";
+import { test } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    mesage: "Hello World",
-  });
-});
+router.get("/test", test);
+
 export default router;
