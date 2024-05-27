3b67eb66d0aeb24f9ef0dbd377626cd8d7ffb4a2
DivineLeafBird <kassimyahya93@gmail.com>
2024-05-27 14:27:50 +0300

    chore: Add signup route and controller for user authentication

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 12);
+  const newUser = new User({
+    firstname,
+    lastname,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully!");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
