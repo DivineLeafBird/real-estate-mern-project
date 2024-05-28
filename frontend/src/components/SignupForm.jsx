747c41bf6762f00aaeb1dcbd7c39c910aad4fa20
DivineLeafBird <kassimyahya93@gmail.com>
2024-05-28 12:36:13 +0300

    chore: Refactor user authentication and API endpoints

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaApple, FaRegEye } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
 import { PiEyeClosedLight } from "react-icons/pi";
@@ -12,10 +12,98 @@ const SignupForm = ({ show, onClose }) => {
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
-  const [comfirmPasswordVisible, setComfirmPasswordVisible] = useState(false);
-  const toggleComfirmPasswordVisibility = () => {
-    setComfirmPasswordVisible(!comfirmPasswordVisible);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+  const toggleConfirmPasswordVisibility = () => {
+    setConfirmPasswordVisible(!confirmPasswordVisible);
   };
+
+  // State for form data and errors
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  // Handle input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // Update form data
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    // Validate input and update errors
+    let newErrors = { ...errors };
+    if (name === "firstname") {
+      if (value.length < 3) {
+        newErrors[name] = "Name must be at least 3 characters long.";
+      } else {
+        newErrors[name] = "";
+      }
+    } else if (name === "lastname") {
+      if (value.length < 3) {
+        newErrors[name] = "Name must be at least 3 characters long.";
+      } else {
+        newErrors[name] = "";
+      }
+    } else if (name === "email") {
+      const emailPattern = /\S+@\S+\.\S+/;
+      if (!emailPattern.test(value)) {
+        newErrors[name] = "Email is invalid.";
+      } else {
+        newErrors[name] = "";
+      }
+    } else if (name === "password") {
+      if (value.length < 8) {
+        newErrors[name] = "Password must be at least 8 characters long.";
+      } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(value)) {
+        newErrors[name] =
+          "Password must contain at least one special character.";
+      } else {
+        newErrors[name] = "";
+      }
+    } else if (name === "confirmPassword") {
+      if (value !== formData.password) {
+        newErrors[name] = "Passwords do not match.";
+      } else {
+        newErrors[name] = "";
+      }
+    }
+
+    setErrors(newErrors);
+  };
+
+  // Check if the form is valid
+  useEffect(() => {
+    const formIsValid =
+      Object.values(formData).every((value) => value !== "") &&
+      Object.values(errors).every((error) => error === "");
+    setIsFormValid(formIsValid);
+  }, [formData, errors]);
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isFormValid) {
+      // Handle form submission
+      console.log("Form submitted", formData);
+    } else {
+      console.log("Form has errors");
+    }
+  };
+
   return (
     <div
       className={`fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50 ${
@@ -32,42 +120,76 @@ const SignupForm = ({ show, onClose }) => {
         <h2 className="text-2xl font-bold mb-4 text-center text-darkblue">
           Create a new Account
         </h2>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <input
               required
-              name="first-name"
               type="text"
+              name="firstname"
+              value={formData.firstname}
+              onChange={handleChange}
               placeholder="First Name"
-              className="px-3 py-2 border font-normal focus:outline-none rounded w-full"
+              className={`px-3 py-2 border font-normal focus:outline-none rounded w-full ${
+                errors.firstname ? "border-red-500" : "focus:border-green-500"
+              } `}
             />
+            {errors.firstname && (
+              <span className="text-red-500 text-xs  mt-2">
+                {errors.firstname}
+              </span>
+            )}
           </div>
           <div className="mb-4">
             <input
               required
               type="text"
-              name="last-name"
+              name="lastname"
+              value={formData.lastname}
+              onChange={handleChange}
               placeholder="Last Name"
-              className="px-3 py-2 border font-normal focus:outline-none rounded w-full"
+              className={`px-3 py-2 border font-normal focus:outline-none rounded w-full ${
+                errors.lastname ? "border-red-500" : "focus:border-green-500"
+              } `}
             />
+            {errors.lastname && (
+              <span className="text-red-500 text-xs mt-2">
+                {errors.lastname}
+              </span>
+            )}
           </div>
           <div className="mb-4">
             <input
               required
               type="email"
               name="email"
+              value={formData.email}
+              onChange={handleChange}
               placeholder="Email"
-              className="px-3 py-2 border font-normal focus:outline-none rounded w-full"
+              className={`px-3 py-2 border font-normal focus:outline-none rounded w-full ${
+                errors.email ? "border-red-500" : "focus:border-green-500"
+              } `}
             />
+            {errors.email && (
+              <span className="text-red-500 text-xs  mt-2">{errors.email}</span>
+            )}
           </div>
           <div className="mb-4 relative">
             <input
               required
               type={passwordVisible ? "text" : "password"}
               name="password"
-              className="px-3 py-2 border font-normal focus:outline-none rounded w-full"
+              value={formData.password}
+              onChange={handleChange}
+              className={`px-3 py-2 border font-normal focus:outline-none rounded w-full ${
+                errors.password ? "border-red-500" : "focus:border-green-500"
+              } `}
               placeholder="Password"
             />
+            {errors.password && (
+              <span className="text-red-500 text-xs  mt-2">
+                {errors.password}
+              </span>
+            )}
             <span
               className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
               onClick={togglePasswordVisibility}
@@ -82,16 +204,27 @@ const SignupForm = ({ show, onClose }) => {
           <div className="mb-4 relative">
             <input
               required
-              type={comfirmPasswordVisible ? "text" : "password"}
-              name="comfirm-password"
-              className="px-3 py-2 border font-normal focus:outline-none rounded w-full"
+              type={confirmPasswordVisible ? "text" : "password"}
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              className={`px-3 py-2 border font-normal focus:outline-none rounded w-full ${
+                errors.confirmPassword
+                  ? "border-red-500"
+                  : "focus:border-green-500"
+              } `}
               placeholder="Confirm Password"
             />
+            {errors.confirmPassword && (
+              <span className="text-red-500 text-xs mt-2">
+                {errors.confirmPassword}
+              </span>
+            )}
             <span
               className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
-              onClick={toggleComfirmPasswordVisibility}
+              onClick={toggleConfirmPasswordVisibility}
             >
-              {comfirmPasswordVisible ? (
+              {confirmPasswordVisible ? (
                 <FaRegEye className="w-6 h-6 text-darkblue" />
               ) : (
                 <PiEyeClosedLight className="w-6 h-6 text-darkblue" />
@@ -113,7 +246,11 @@ const SignupForm = ({ show, onClose }) => {
               <Link className="text-skyblue">Privacy Policy.</Link>
             </span>
           </div>
-          <button className="bg-blue hover:bg-softgray text-white font-medium py-2 px-4 rounded w-full">
+          <button
+            type="submit"
+            disabled={!isFormValid}
+            className="bg-blue disabled:bg-softgray disabled:opacity-50  text-white font-medium py-2 px-4 rounded w-full"
+          >
             Sign Up
           </button>
         </form>
@@ -144,6 +281,7 @@ const SignupForm = ({ show, onClose }) => {
           </button>
           <button
             type="submit"
+            disabled
             className="w-full disabled:hidden flex items-center justify-around border text-base mt-5 py-2 px-8 gap-4 rounded"
           >
             <div>
@@ -157,6 +295,7 @@ const SignupForm = ({ show, onClose }) => {
           </button>
           <button
             type="submit"
+            disabled
             className="w-full disabled:hidden flex items-center justify-around border text-base mt-5 py-2 px-8 gap-4 rounded"
           >
             <FaApple className="w-6 h-6" />
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,24 +1,112 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import { FaApple, FaRegEye } from "react-icons/fa6";
 import { PiEyeClosedLight } from "react-icons/pi";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import { IoClose } from "react-icons/io5";
 import Google from "../assets/icons/google.png";
 import Facebook from "../assets/icons/facebook.png";
 
 const SignUp = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
+
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
-  const [comfirmPasswordVisible, setComfirmPasswordVisible] = useState(false);
-  const toggleComfirmPasswordVisibility = () => {
-    setComfirmPasswordVisible(!comfirmPasswordVisible);
+
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+  const toggleConfirmPasswordVisibility = () => {
+    setConfirmPasswordVisible(!confirmPasswordVisible);
   };
+
   const navigate = useNavigate();
 
+  // State for form data and errors
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  // Handle input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    // Update form data
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    // Validate input and update errors
+    let newErrors = { ...errors };
+    if (name === "firstname") {
+      if (value.length < 3) {
+        newErrors[name] = "Name must be at least 3 characters long.";
+      } else {
+        newErrors[name] = "";
+      }
+    } else if (name === "lastname") {
+      if (value.length < 3) {
+        newErrors[name] = "Name must be at least 3 characters long.";
+      } else {
+        newErrors[name] = "";
+      }
+    } else if (name === "email") {
+      const emailPattern = /\S+@\S+\.\S+/;
+      if (!emailPattern.test(value)) {
+        newErrors[name] = "Email is invalid.";
+      } else {
+        newErrors[name] = "";
+      }
+    } else if (name === "password") {
+      if (value.length < 8) {
+        newErrors[name] = "Password must be at least 8 characters long.";
+      } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(value)) {
+        newErrors[name] =
+          "Password must contain at least one special character.";
+      } else {
+        newErrors[name] = "";
+      }
+    } else if (name === "confirmPassword") {
+      if (value !== formData.password) {
+        newErrors[name] = "Passwords do not match.";
+      } else {
+        newErrors[name] = "";
+      }
+    }
+
+    setErrors(newErrors);
+  };
+
+  // Check if the form is valid
+  useEffect(() => {
+    const formIsValid =
+      Object.values(formData).every((value) => value !== "") &&
+      Object.values(errors).every((error) => error === "");
+    setIsFormValid(formIsValid);
+  }, [formData, errors]);
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isFormValid) {
+      // Handle form submission
+      console.log("Form submitted", formData);
+    } else {
+      console.log("Form has errors");
+    }
+  };
   return (
     <>
       <Header />
@@ -34,42 +122,85 @@ const SignUp = () => {
             <h2 className="text-2xl font-bold mb-4 text-center text-darkblue">
               Create a new Account
             </h2>
-            <form action="">
+            <form onSubmit={handleSubmit}>
               <div className="mb-4">
                 <input
                   required
-                  name="first-name"
                   type="text"
+                  name="firstname"
+                  value={formData.firstname}
+                  onChange={handleChange}
+                  className={`px-3 py-2 border font-normal focus:outline-none rounded w-full ${
+                    errors.firstname
+                      ? "border-red-500"
+                      : "focus:border-green-500"
+                  } `}
                   placeholder="First Name"
-                  className="px-3 py-2 border font-normal focus:outline-none rounded w-full"
                 />
+                {errors.firstname && (
+                  <span className="text-red-500 text-xs  mt-2">
+                    {errors.firstname}
+                  </span>
+                )}
               </div>
               <div className="mb-4">
                 <input
                   required
                   type="text"
-                  name="last-name"
+                  name="lastname"
+                  value={formData.lastname}
+                  onChange={handleChange}
+                  className={`px-3 py-2 border font-normal focus:outline-none rounded w-full ${
+                    errors.lastname
+                      ? "border-red-500"
+                      : "focus:border-green-500"
+                  } `}
                   placeholder="Last Name"
-                  className="px-3 py-2 border font-normal focus:outline-none rounded w-full"
                 />
+                {errors.lastname && (
+                  <span className="text-red-500 text-xs mt-2">
+                    {errors.lastname}
+                  </span>
+                )}
               </div>
               <div className="mb-4">
                 <input
                   required
                   type="email"
                   name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className={`px-3 py-2 border font-normal focus:outline-none rounded w-full ${
+                    errors.email ? "border-red-500" : "focus:border-green-500"
+                  } `}
                   placeholder="Email"
-                  className="px-3 py-2 border font-normal focus:outline-none rounded w-full"
                 />
+
+                {errors.email && (
+                  <span className="text-red-500 text-xs  mt-2">
+                    {errors.email}
+                  </span>
+                )}
               </div>
               <div className="mb-4 relative">
                 <input
                   required
                   type={passwordVisible ? "text" : "password"}
                   name="password"
-                  className="px-3 py-2 border font-normal focus:outline-none rounded w-full"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className={`px-3 py-2 border font-normal focus:outline-none rounded w-full ${
+                    errors.password
+                      ? "border-red-500"
+                      : "focus:border-green-500"
+                  } `}
                   placeholder="Password"
                 />
+                {errors.password && (
+                  <span className="text-red-500 text-xs  mt-2">
+                    {errors.password}
+                  </span>
+                )}
                 <span
                   className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
                   onClick={togglePasswordVisibility}
@@ -84,16 +215,27 @@ const SignUp = () => {
               <div className="mb-4 relative">
                 <input
                   required
-                  type={comfirmPasswordVisible ? "text" : "password"}
-                  name="comfirm-password"
-                  className="px-3 py-2 border font-normal focus:outline-none rounded w-full"
+                  type={confirmPasswordVisible ? "text" : "password"}
+                  name="confirmPassword"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  className={`px-3 py-2 border font-normal focus:outline-none rounded w-full ${
+                    errors.confirmPassword
+                      ? "border-red-500"
+                      : "focus:border-green-500"
+                  } `}
                   placeholder="Confirm Password"
                 />
+                {errors.confirmPassword && (
+                  <span className="text-red-500 text-xs mt-2">
+                    {errors.confirmPassword}
+                  </span>
+                )}
                 <span
                   className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
-                  onClick={toggleComfirmPasswordVisibility}
+                  onClick={toggleConfirmPasswordVisibility}
                 >
-                  {comfirmPasswordVisible ? (
+                  {confirmPasswordVisible ? (
                     <FaRegEye className="w-6 h-6 text-darkblue" />
                   ) : (
                     <PiEyeClosedLight className="w-6 h-6 text-darkblue" />
@@ -117,7 +259,10 @@ const SignUp = () => {
                   <Link className="text-skyblue">Privacy Policy.</Link>
                 </span>
               </div>
-              <button className="bg-blue hover:bg-softgray text-white font-medium py-2 px-4 rounded w-full">
+              <button
+                disabled={!isFormValid}
+                className="bg-blue disabled:bg-softgray disabled:opacity-50 text-white font-medium py-2 px-4 rounded w-full"
+              >
                 Sign Up
               </button>
             </form>
